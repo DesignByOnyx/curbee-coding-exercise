9f6ae9d19510bc22a746c0c62c344a4e9c6bef1f
Ryan Wheale <ryan.wheale@gmail.com>
2025-03-31 09:53:42 -0600

    feat: add rest endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
-import { Hono } from 'hono';
+import { Context, Hono } from 'hono';
 import { trpcServer } from '@hono/trpc-server';
 import { AppointmentRouter } from './routes/appointment/appointment.js';
 import { router } from './trpc.js';
+import { IAppointmentCreate } from './schemas/appointment.schema.js';
+import { TRPCError } from '@trpc/server';
+import { ContentfulStatusCode } from 'hono/utils/http-status';
 
 const ApiRouter = router({
   appointment: AppointmentRouter,
@@ -22,6 +25,69 @@ api.get('/', (c) => {
   `)
 });
 
+/** Mapping of tRPC error codes to HTTP status codes */
+const ErrorStatus: Record<string, ContentfulStatusCode> = {
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INTERNAL_SERVER_ERROR: 500,
+  SERVICE_UNAVAILABLE: 503,
+};
+
+/** Represents an error response JSON */
+type ErrorResponse = {
+  error: {
+    message: string;
+    status: ContentfulStatusCode;
+  };
+};
+
+/** Formats REST errors to a consistent JSON shape */
+const formatRestError = (err: unknown): ErrorResponse => {
+  if (err instanceof TRPCError) {
+    return {
+      error: {
+        message: err.message,
+        status: ErrorStatus[err.code] || 500,
+      },
+    };  
+  }
+
+  return {
+    error: {
+      message: `Unknown Server Error: ${err}`,
+      status: 500,
+    },
+  };
+};
+
+/** Handles calling async REST middleware and consistently handling the output */
+const handleRest = (fn: (c: Context) => Promise<Response>) => async (c: Context) => {
+  try {
+    return await fn(c);
+  } catch (err) {
+    const formattedError = formatRestError(err);
+    return c.json(formattedError, formattedError.error.status);
+  }
+};
+
+api.get('/rest/appointment', handleRest(async (c) => {
+  const caller = AppointmentRouter.createCaller({} as any);
+  return c.json(await caller.find());
+}));
+
+api.get('/rest/appointment/:id', handleRest(async (c) => {
+  const caller = AppointmentRouter.createCaller({} as any);
+  return c.json(await caller.get(c.req.param('id')));
+}));
+
+api.post('/rest/appointment', handleRest(async (c: Context) => {
+  const caller = AppointmentRouter.createCaller({} as any);
+  const data = await c.req.json<IAppointmentCreate>();
+  return c.json(await caller.create(data));
+}));
 
 export default api;
 export type IApiRouter = typeof ApiRouter;
